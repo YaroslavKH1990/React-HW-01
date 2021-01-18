3377141bc2e926a23a86c7714239b8e9dea35de9
YaroslavKH1990 <yaroslav.kh.ua@gmail.com>
2021-01-18 19:09:18 +0200

    commit

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import StatList from './StatList.jsx';
 import style from '../styles/statistics.module.scss';
 
-const Statistics = ({ stats }) => {
+const Statistics = ({ stats, title }) => {
   return (
     <>
       <div className={style.wrapper}>
-        {stats.length > 0 && <h2 className={style.title}>Upload stats</h2>}
+        {title && <h2 className={style.title}>{title}</h2>}
         <ul className={style.statList}>
           {stats.map(stat => (
             <StatList key={stat.id} stat={stat} />
